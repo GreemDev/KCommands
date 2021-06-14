1a1fd7a1ab474a86ca6a271d4a1ee7114849e8ce
GreemDev <gr33m11@gmail.com>
2021-06-14 14:26:40 -0500

    jitpack(?)

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -2,9 +2,13 @@ plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm") version "1.4.31"
 
+    `maven-publish`
+
     // Apply the java-library plugin for API and implementation separation.
     `java-library`
 }
+
+group = "com.github.GreemDev"
 
 repositories {
     mavenCentral()
